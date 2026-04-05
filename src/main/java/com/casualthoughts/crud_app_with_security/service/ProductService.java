0f8b9c81bd3a78480a0bf0ed01e56e8d05b4ae03
Prajwal8197 <prawalpraju453@gmail.com>
2026-04-05 18:59:03 +0530

    feat(product): implement pagination and bulk addition for products

--- src/main/java/com/casualthoughts/crud_app_with_security/service/ProductService.java
+++ src/main/java/com/casualthoughts/crud_app_with_security/service/ProductService.java
@@ -1,8 +1,15 @@
 package com.casualthoughts.crud_app_with_security.service;
 
+import com.casualthoughts.crud_app_with_security.dto.GenericApiResponse;
 import com.casualthoughts.crud_app_with_security.entity.Product;
 import com.casualthoughts.crud_app_with_security.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -13,5 +20,13 @@ public class ProductService {
 
     public List<Product> getProducts() { return repository.findAll(); }
     public Product getProduct(Long id) { return repository.findById(id).orElse(null); }
-    public Product saveProduct(Product product) { return repository.save(product); }
+    public List<Product> saveProduct(List<Product> products) { return repository.saveAll(products); }
+
+    public ResponseEntity<GenericApiResponse<?>> getProductPage(Pageable pageable) {
+
+        Page<Product> productPage = repository.findAll(pageable);
+
+        return GenericApiResponse.success("Products fetched Succesfully",200, productPage);
+    }
+
 }
